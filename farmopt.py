5473a108a0d5de49f608431f35742018a7252ac8
SelinaWang <ziyu.wang@students.olin.edu>
2015-11-17 01:33:17 -0500

    Setting up app and config

--- farmopt.py
+++ farmopt.py
@@ -2,8 +2,24 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
-from app_config import *
+# from app_config import *
+
+# configuration
+DATABASE = '/tmp/farmopt.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
 
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
+# FARMOPT_SETTINGS = app_config
+# app.config.from_envvar('FARMOPT_SETTINGS', silent=True)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    app.run()
