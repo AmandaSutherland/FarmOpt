f8403650ce5dd3567664e2f269f5efe85885f02e
Amanda Sutherland <amanda.h.sutherland.93@gmail.com>
2015-12-01 17:44:01 -0500

    adding to gitignore

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/selina/Documents/SoftDes/SoftwareDesignFall15/FarmOpt/venv"
+set -gx VIRTUAL_ENV "/home/rapunzel/SoftDes/FarmOpt/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
