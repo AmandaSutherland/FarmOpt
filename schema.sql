0a735d342371e519005b695ecf5a4104ffd0840e
SelinaWang <ziyu.wang@students.olin.edu>
2015-12-04 16:58:13 -0500

    Adding multiple tables in database

--- schema.sql
+++ schema.sql
@@ -5,3 +5,16 @@ create table entries (
   text text not null
 );
 
+drop table if exists crops;
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  text text not null
+);
+
+drop table if exists weeks;
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  text text not null
+);
