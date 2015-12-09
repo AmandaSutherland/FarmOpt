7452decb88fea417f09a815dd5f993afd04b5510
Amanda Sutherland <amanda.h.sutherland.93@gmail.com>
2015-12-08 20:00:37 -0500

    plotting does not yet work.

--- farmopt.py
+++ farmopt.py
@@ -3,6 +3,10 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, Response
 from contextlib import closing
+import pygal
+import json
+from urllib2 import urlopen  # python 2 syntax
+from pygal.style import DarkSolarizedStyle
 # from flask_googlelogin import GoogleLogin
 # from flask_login import LoginManager, login_required
 
@@ -70,6 +74,18 @@ def crops():
     cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('crop_page.html', entries=['please', 'link', 'me'], crop_dictionary={'carrots': '3', 'beans': '2'})
+
+@app.route('/chart')
+def plot_chart(date='20140415', state='IA', city='Ames'):
+    line_chart = pygal.Line()
+    line_chart.title = 'Browser usage evolution (in %)'
+    line_chart.x_labels = map(str, range(2002, 2013))
+    line_chart.add('Firefox', [None, None,    0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+    line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
+    line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+    line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
+    line_chart = line_chart.render()
+    return render_template('plot.html', line_chart=line_chart)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
