bd2b67f0fbd5d06f8864f0bb0d4a5f284e060223
SelinaWang <ziyu.wang@students.olin.edu>
2015-12-18 06:45:07 -0500

    Deploying to heroku

--- app_config.py
+++ app_config.py
@@ -1,5 +1,5 @@
 # configuration
-DATABASE = '/tmp/farmopt.db'
+DATABASE = 'postgres://txeridpifjdxex:ENCmuGTme3kX78VsubI2HOn8Dk@ec2-54-83-204-228.compute-1.amazonaws.com:5432/d31jftgnv9ipu1'
 DEBUG = True
 SECRET_KEY = 'development key'
 # USERNAME = 'admin'
